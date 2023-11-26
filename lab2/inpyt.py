79dd995d3ece43417d582116ba1f207f52cf7d4e
Elfrida <elfrida.shashkevich@gmail.com>
2023-11-26 14:28:44 +0300

    lab 2, add json

--- lab2/inpyt.py
+++ lab2/inpyt.py
@@ -1,6 +1,12 @@
 import dominant_strategy as ds
 import numpy as np
+import json
 
-matrix = np.array([[8,9,9,4], [6,5,8,7],[3,4,8,6], [8,9,9,4]])
+#matrix = np.array([[8,9,9,4], [6,5,8,7],[3,4,8,6], [8,9,9,4]])
+json_str= open('input.json', 'r', encoding='utf-8').read()
+data = json.loads(json_str)
+matrix = data['matrix']
 
 ds.dominant_strategy(matrix)
+
+
